3913af0faff992737ddd2d1edcfcc00fb508f605
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2022-07-16 12:48:53 -0400

    added dom selectors

---
+++ week1/index.js
@@ -0,0 +1,8 @@
+
+const input = document.getElementById('input');
+const numbers = document.querySelectorAll('.numbers div');
+const operators = document.querySelectorAll('.operators div')
+const result = document.getElementById('result');
+const clear = document.getElementById('clear');
+
+
