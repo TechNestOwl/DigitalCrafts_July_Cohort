51c4b98e3f19b6e6e1b482e39bcce5389a159068
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2022-07-16 13:08:36 -0400

    click handlers

--- week1/index.js
+++ week1/index.js
@@ -5,4 +5,19 @@ const operators = document.querySelectorAll('.operators div')
 const result = document.getElementById('result');
 const clear = document.getElementById('clear');
 
+let resultDisplayed = false // this is just a flag to keep an eye on what is displayed
 
+//add click handlers to the numbers buttons
+for (let i = 0 ; i < numbers.length; i++) {
+    number[i].addEventListener('click', function(e){
+
+        //sotring current input string and its last character in variables 
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+
+        if(resultDisplayed === false){
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    })
+};
