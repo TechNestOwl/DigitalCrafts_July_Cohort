b858b4e7be1de330197ea5d46db4780933635673
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2022-07-19 12:14:38 -0400

    added .forEach exercise

--- week1/arrayMethods.js
+++ week1/arrayMethods.js
@@ -1,13 +1,25 @@
 // console.log('heelo world')
 
+//.forEach does exactily what it's name suggestes
+
+const numbers = [1,2,3,4,5];
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// };
+
+numbers.forEach(function(num){
+    console.log(num)
+})
+
+
 
 //.filter iterates through an array and only returns data that meaets the criteria(defined trough a truth test inside a function)
-const words = ['cat','discombobulate', 'dog', 'notarealword','pet','computer'];
 
+const words = ['cat','discombobulate', 'dog', 'notarealword','pet','computer'];
 const threeLetterWord = words.filter(function(word){
     return word.length === 3;
 });
-console.log(threeLetterWord);
+// console.log(threeLetterWord);
 
 
 const people = [
@@ -16,8 +28,7 @@ const people = [
     {name:'jake', age: 25, home:'Leeds'},
     {name:'stephan', age: 25, home:'Atlanta'},
 ];
-
 const leedsPeeps = people.filter(function(person){
     return person.home === "Leeds";
 });
-console.log(leedsPeeps);
+// console.log(leedsPeeps);
